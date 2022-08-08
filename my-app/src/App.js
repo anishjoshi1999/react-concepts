20523dd46f86e6919cd5076ef991e16da707ea56
anishjoshi1999 <anishjoshi1999@gmail.com>
2022-08-08 15:26:19 +0545

    Tours

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,45 +1,54 @@
-import React, {useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react'
+import Loading from './Loading'
+import Tours from './Tours'
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = 'https://course-api.com/react-tours-project'
 function App() {
-  const [windowWidth, setwindowWidth] = useState(window.innerWidth)
-  const handleResize = () => {
-    setwindowWidth(window.innerWidth)
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id)
+    setTours(newTours)
   }
-  useEffect(()=>{
-    window.addEventListener('resize',handleResize)
 
-    return ()=> {
-      window.addEventListener('resize',handleResize)
+  const fetchTours = async () => {
+    setLoading(true)
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false)
+      setTours(tours)
+      console.log(tours)
+    } catch (error) {
+      setLoading(false)
+      console.log(error)
     }
-  },[])
-
-  return(
-    <div>{windowWidth}</div>
+  }
+  useEffect(() => {
+    fetchTours()
+  }, [])
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    )
+  }
+  if(tours.length === 0){
+    return <main>
+      <div className="title">
+        <h2>no tours left</h2>
+        <button className='btn' onClick={()=>{fetchTours()}}>refresh</button>
+      </div>
+    </main>
+  }
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour}/>
+    </main>
   )
 }
 
-// First example of useState
-// function App() {
-//   const [resourceType, setResourceType] = useState('posts')
-//   const [items,setItems] = useState([])
-
-//   useEffect(()=>{
-//     fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
-//     .then(response => response.json())
-//     .then(json=> setItems(json))
-//   },[resourceType])
-
-//   return(
-//     <>
-//     <div>
-//       <button onClick={()=> setResourceType('posts')}>Posts</button>
-//       <button onClick={()=> setResourceType('users')}>Users</button>
-//       <button onClick={()=> setResourceType('comments')}>Comments</button>
-//     </div>
-//     <h1>{resourceType}</h1>
-//     {items.map(item => {
-//       return <pre>{JSON.stringify(item)}</pre>
-//     })}
-//       </>
-//   )
-// }
-export default App;
+export default App
