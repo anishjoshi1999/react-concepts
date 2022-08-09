2d5415186c045661369665fcfcb65ccc5b0b7f7a
anishjoshi1999 <anishjoshi1999@gmail.com>
2022-08-09 17:09:17 +0545

    accordion

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,45 +1,22 @@
-import React, {useState,useEffect} from 'react';
+import React, { useState } from 'react';
+import data from './data';
+import SingleQuestion from './Question';
 function App() {
-  const [windowWidth, setwindowWidth] = useState(window.innerWidth)
-  const handleResize = () => {
-    setwindowWidth(window.innerWidth)
-  }
-  useEffect(()=>{
-    window.addEventListener('resize',handleResize)
-
-    return ()=> {
-      window.addEventListener('resize',handleResize)
-    }
-  },[])
-
-  return(
-    <div>{windowWidth}</div>
-  )
+  const [questions, setQuestions] = useState(data);
+  return (
+    <main>
+      <div className='container'>
+        <h3>questions and answers about login</h3>
+        <section className='info'>
+          {questions.map((question) => {
+            return (
+              <SingleQuestion key={question.id} {...question}></SingleQuestion>
+            );
+          })}
+        </section>
+      </div>
+    </main>
+  );
 }
 
-// First example of useState
-// function App() {
-//   const [resourceType, setResourceType] = useState('posts')
-//   const [items,setItems] = useState([])
-
-//   useEffect(()=>{
-//     fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
-//     .then(response => response.json())
-//     .then(json=> setItems(json))
-//   },[resourceType])
-
-//   return(
-//     <>
-//     <div>
-//       <button onClick={()=> setResourceType('posts')}>Posts</button>
-//       <button onClick={()=> setResourceType('users')}>Users</button>
-//       <button onClick={()=> setResourceType('comments')}>Comments</button>
-//     </div>
-//     <h1>{resourceType}</h1>
-//     {items.map(item => {
-//       return <pre>{JSON.stringify(item)}</pre>
-//     })}
-//       </>
-//   )
-// }
 export default App;
