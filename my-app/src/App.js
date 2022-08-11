fd67ff56bd8494fe45be4995561f104e0c99529c
anishjoshi1999 <anishjoshi1999@gmail.com>
2022-08-11 10:23:10 +0545

    6-Tabs Display First Job

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,45 +1,49 @@
-import React, {useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react'
+import { FaAngleDoubleRight } from 'react-icons/fa'
+
+const url = 'https://course-api.com/react-tabs-project'
 function App() {
-  const [windowWidth, setwindowWidth] = useState(window.innerWidth)
-  const handleResize = () => {
-    setwindowWidth(window.innerWidth)
+  const [loading, setloading] = useState(true)
+  const [jobs, setjobs] = useState([])
+  const [value, setvalue] = useState(0)
+
+  const fetchJobs = async () => {
+    const response = await fetch(url)
+    const newJobs = await response.json()
+    setjobs(newJobs)
+    setloading(false)
   }
-  useEffect(()=>{
-    window.addEventListener('resize',handleResize)
-
-    return ()=> {
-      window.addEventListener('resize',handleResize)
-    }
+  // runs only when our app renders
+  useEffect(()=> {
+    fetchJobs()
   },[])
-
-  return(
-    <div>{windowWidth}</div>
-  )
+  if(loading){
+    return <section className='section loading'>
+      <h1>loading...</h1>
+    </section>
+  }
+  const {company,dates,duties, title} = jobs[value];
+  return <section className='section'>
+    <div className="title">
+      <h2>experience</h2>
+      <div className="underline"></div>
+    </div>
+    <div className="jobs-center">
+      {/* {btn container} */}
+      {/* {job info} */}
+      <article className="job-info">
+        <h3>{title}</h3>
+        <h4>{company}</h4>
+        <p className="job-date">{dates}</p>
+        {duties.map((duty,index)=>{
+          return <div key={index} className="job-desc">
+            <FaAngleDoubleRight className='job-icon'></FaAngleDoubleRight>
+            <p>{duty}</p>
+          </div>
+        })}
+      </article>
+    </div>
+  </section>
 }
 
-// First example of useState
-// function App() {
-//   const [resourceType, setResourceType] = useState('posts')
-//   const [items,setItems] = useState([])
-
-//   useEffect(()=>{
-//     fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
-//     .then(response => response.json())
-//     .then(json=> setItems(json))
-//   },[resourceType])
-
-//   return(
-//     <>
-//     <div>
-//       <button onClick={()=> setResourceType('posts')}>Posts</button>
-//       <button onClick={()=> setResourceType('users')}>Users</button>
-//       <button onClick={()=> setResourceType('comments')}>Comments</button>
-//     </div>
-//     <h1>{resourceType}</h1>
-//     {items.map(item => {
-//       return <pre>{JSON.stringify(item)}</pre>
-//     })}
-//       </>
-//   )
-// }
-export default App;
+export default App
